0de8b9eb90dd071f5debddcc773a189ed592541c
ayan471 <pakhiraayan@gmail.com>
2023-03-01 11:47:26 +0530

    built the view cart button

--- screens/ResturantDetail.js
+++ screens/ResturantDetail.js
@@ -3,14 +3,16 @@ import React from "react";
 import { Divider } from "react-native-elements";
 import About from "../components/resturantDetail/About";
 import MenuItems from "../components/resturantDetail/MenuItems";
+import ViewCart from "../components/resturantDetail/ViewCart";
 
-export default function ResturantDetail({ route }) {
+export default function ResturantDetail({ route, navigation }) {
   return (
     <View>
       <ScrollView showsVerticalScrollIndicator={false}>
         <About route={route} />
         <Divider width={1.8} style={{ marginVertical: 20 }} />
         <MenuItems />
+        <ViewCart navigation={navigation} resturantName={route.params.name} />
       </ScrollView>
     </View>
   );
